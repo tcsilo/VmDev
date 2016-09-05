11d70a01907cb12cbaa69b1e231f113ea09e10e4
tcsilo <mtuliocastillo@gmail.com>
2016-09-05 00:48:52 -0300

    Modal status update

--- Angular-Prueba NPM/js/angular/app.js
+++ Angular-Prueba NPM/js/angular/app.js
@@ -1,6 +1,8 @@
 (function() {
     var app = angular.module("app", []);
 
+    var Data = {};
+
     app.controller("HomeController", ['$http', function($http) {
         var self = this;
         self.listaArticulos = [{ "titulo": "Nulla ac enim.", "texto": "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.", "comentarios": ["Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.", "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.", "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem."] }];
@@ -12,24 +14,49 @@
 
     app.controller("TestController", ['$http', function($http) {
         var self = this;
-        self.list = {};
-        self.postToShow = 5;
+        self.postData = Data;
+        self.postData.posts = {};
+        self.postData.comments = {};
+        self.postData.postToShow = 5;
 
-        self.LoadMorePosts = function() {
-            self.postToShow += 5;
+        self.postData.LoadMorePosts = function() {
+            self.postData.postToShow += 5;
 
         };
 
         $http.get('https://jsonplaceholder.typicode.com/posts').success(function(data) {
             if (data.constructor === Array) {
-                data.varType = "ArrayPlaced";
-                self.list = data;
+                self.postData.posts = data.slice(0, 10);
             } else {
-                data.varType = "ArrayPushed";
-                self.list.push(data);
+                self.postData.posts.push(data);
             }
 
         });
+
+        $http.get('https://jsonplaceholder.typicode.com/comments').success(function(data) {
+            if (data.constructor === Array) {
+                self.postData.comments = data.slice(0, 10);
+            } else {
+                self.postData.comments.push(data);
+            }
+
+        });
+        // Data = self.postData;
+    }]);
+
+    app.controller('PostController', ['$http', function() {
+        var self = this;
+        self.status = "";
+        self.PostStatusUpdate = function(status) {
+            Data.posts.unshift({
+                "userId": "30",
+                "id": "30",
+                "title": "New status update!",
+                "body": self.status,
+                "image" : ""
+            });
+        };
+
     }]);
 
     app.controller("PicChangeController", ['$http', function($http) {
