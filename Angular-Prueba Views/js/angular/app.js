341df36654032cd1b485ef2a264ec87bc9575d60
Tulio Castillo <tcastillo@virtualmind.net>
2016-09-06 17:04:07 -0300

    Next iteration

--- Angular-Prueba Views/js/angular/app.js
+++ Angular-Prueba Views/js/angular/app.js
@@ -1,87 +1,118 @@
 (function() {
-        var app = angular.module("app", []);
+    var app = angular.module("app", []);
 
-        var Data = {};
+    var Data = {};
+    var showFeed = { Featured: true, Stories: false, Saved: false };
 
-        app.controller("HomeController", ['$http', '$scope', function($http, $scope) {
-            var self = this;
-            self.Data = Data;
-            // self.Data.posts = [{ "likes": 0, "titulo": "Nulla ac enim.", "texto": "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.", "comentarios": ["Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.", "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.", "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem."] }];
-            var comments = {};
+    app.controller("HomeController", ['$scope', 'jsonService', function($scope, jsonService) {
+        var self = this;
+        self.Data = Data;
+        self.dataLoaded = false;
 
-            $http.get('https://jsonplaceholder.typicode.com/posts').success(function(data) {
+        function LoadRandomPosts(argument) {
+        	self.dataLoaded = false;
+        	var mxPost = Math.floor(Math.random() * Data.allPosts.length);
+            var mnPost = Math.floor(Math.random() * mxPost);
+            Data.posts = Data.allPosts.slice(mnPost, mxPost);
+            LoadComments();
+            self.dataLoaded = true;
+        }
 
-                self.Data.posts = data.slice(0, 10);
-            });
+        function LoadComments() {
+            Data.posts = Data.posts.map(function(arg) {
+                var maxComments = Math.floor(Math.random() * 3);
+                var nComment = 0;
 
-            $http.get('https://jsonplaceholder.typicode.com/comments').success(function(data) {
-                comments = data;
+                arg.comments = Data.comments.filter(function(com) {
+                    if (com.postId == arg.id) {
+                        nComment++;
 
-                self.Data.posts = self.Data.posts.map(function(arg) {
-                    var maxComments = Math.floor(Math.random() * 3);
-                    var nComment = 0;
-
-                    arg.comments = comments.filter(function(com) {
-                        if (com.postId == arg.id) {
-                            nComment++;
-
-                            return nComment <= maxComments;
-                        } else
-                            return false;
-                    });
-                    return arg;
+                        return nComment <= maxComments;
+                    } else
+                        return false;
                 });
-                self.Data.posts = self.Data.posts.map(function(arg) {
-                    return { data: arg, likes: 0, newComment: "" };
-                });
-
+                return arg;
             });
+            Data.posts = Data.posts.map(function(arg) {
+                return { data: arg, likes: 0, newComment: "" };
+            });
+        }
 
-            this.plusLikes = function(articulo) {
-                // console.log(articulo);
-                articulo.likes++;
-            };
+        jsonService.getPosts().then(function(payload) {
+            Data.allPosts = payload.data;
+            
 
-            this.plusComment = function(articulo) {
-                // console.log(articulo);
-                if (!articulo.newComment) {return "";}
-                var nCmmt = {};
-                for (var key in articulo.data.comments) {
-                    nCmmt[key] = "";
-                }
-                nCmmt.body = articulo.newComment;
-                articulo.data.comments.push(nCmmt);
-                articulo.newComment = "";
-            };
+            jsonService.getComments().then(function(payload) {
+                // Data.allComments = payload.data;
+                // Data.comments = Data.allComments.slice(0, 50);
 
-            // $http.get('MOCK_DATA.json')
-            //  .success(function(data) {
-            //      console.log("Success");
-            //      // this.Data.posts = data;              
-            //  })
-            //  .error(function() {
-            //      console.log("Failure");
-            //         // this.Data.posts = [{ "titulo": "Nulla ac enim.", "texto": "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.", "comentarios": ["Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.", "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.", "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem."] }];               
-            //     });
-        }]);
+                Data.Comments = payload.data;
+                LoadComments();
 
-        app.controller('PostController', ['$http', function() {
-                var self = this;
-                self.status = "";
-                self.btnText = "Post It!";
-                self.PostStatusUpdate = function(status) {
-                    if (!self.status) {return "";}
-                    Data.posts.unshift({
-                            data: {
-                                title: "New Post!",
-                                body: self.status,
-                                comments: []
-                            },
-                            likes: 0,
-                            newComment: ""                   
-                        }); 
-                        self.status = "";
-                    };
-                }]);
+                self.dataLoaded = true;
+            }, function() {
+                console.log("ERROR COMMENTS");
+            });
+        }, function() {
+            console.log("ERROR POSTS");
+        });
 
-        })();
+        this.Refresh = function() {
+            var mxPost = Math.floor(Math.random() * Data.allPosts.length);
+            var mnPost = Math.floor(Math.random() * mxPost);
+            Data.posts = Data.allPosts.slice(mnPost, mxPost);
+        };
+
+        this.plusLikes = function(articulo) {
+            articulo.likes++;
+        };
+
+        this.plusComment = function(articulo) {
+            if (!articulo.newComment) {
+                return "";
+            }
+            var nCmmt = {};
+            for (var key in articulo.data.comments) {
+                nCmmt[key] = "";
+            }
+            nCmmt.body = articulo.newComment;
+            articulo.data.comments.push(nCmmt);
+            articulo.newComment = "";
+        };
+
+    }]);
+
+    app.controller('PostController', ['$http', function() {
+        var self = this;
+        self.status = "";
+        self.btnText = "Post It!";
+        self.PostStatusUpdate = function(status) {
+            if (!self.status) {
+                return "";
+            }
+            Data.posts.unshift({
+                data: {
+                    title: "New Post!",
+                    body: self.status,
+                    comments: []
+                },
+                likes: 0,
+                newComment: ""
+            });
+            self.status = "";
+        };
+    }]);
+
+    app.factory('jsonService', ['$http', function($http) {
+
+        return {
+            getPosts: function() {
+                return $http.get('https://jsonplaceholder.typicode.com/posts');
+            },
+            getComments: function() {
+                return $http.get('https://jsonplaceholder.typicode.com/comments');
+            }
+        };
+    }]);
+
+})();
